196a301c019133371a5e61701e061abb3bac9c3b
Guilherme Caetano da Silva - 820140489 <guilherme.caetano1010@gmail.com>
2023-09-12 19:42:12 -0300

    Adicionar repetição 1

---
+++ Aula3/repeticao1/Repeticao1.java
@@ -0,0 +1,19 @@
+package Aula3.repeticao1;
+
+import javax.swing.JOptionPane;
+
+public class Repeticao1 {
+        public static void main(String[] args) {
+            double nota1, nota2, media;
+            int contador = 1;
+
+            //while(contador<=5){
+                for(contador = 0; contador <=5; contador++){
+                    nota1 = Double.parseDouble(JOptionPane.showInputDialog("Digite a primeira nota:"));
+                    nota2 = Double.parseDouble(JOptionPane.showInputDialog("Digite a segunda nota:"));
+                    media = (nota1+nota2)/2;
+                    JOptionPane.showMessageDialog(null, "Sua média é igual a: "+media);
+                }
+
+        }
+}
