768676af19151f7e44e4f9448d2ea8b82a3cce80
Guilherme Caetano da Silva - 820140489 <guilherme.caetano1010@gmail.com>
2023-09-12 21:21:28 -0300

    Exercício salário

---
+++ Aula3/salario/Salario.java
@@ -0,0 +1,15 @@
+package Aula3.salario;
+
+
+public class Salario {
+    public static void main(String[] args) {
+        double salario = 1000;
+        double percentual = 1.5;
+        for(int ano = 2006; ano<= 2023; ano++){
+            percentual *= 2;
+            System.out.println("Ano: "+ ano +" Salário: "+salario+ " Percentual do ano seguinte: "+percentual);
+        }
+        System.out.println(salario);
+    }
+
+}
