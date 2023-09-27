5b7637e9ac8909260e11f5c7e28529b06066be99
Guilherme Caetano <106631521+guilhermecaetano1010@users.noreply.github.com>
2023-09-27 19:56:21 -0300

    Update HelloWorld.java

--- Aula1/exemplo1/HelloWorld.java
+++ Aula1/exemplo1/HelloWorld.java
@@ -8,7 +8,6 @@ System.out.println("Digite o valor de uma variável: ");
 Scanner sc = new Scanner(System.in);
 int variavel = sc.nextInt();
 System.out.println(variavel);
-System.out.println(variavel);
 sc.close();
 
 
