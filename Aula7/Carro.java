ba0ef300c3813aebea94736cae3289dc64b9c645
Guilherme444 <guilherme.caetano1010@gmail.com>
2023-09-27 20:07:48 -0300

    Acrescentando aula 7

---
+++ Aula7/Carro.java
@@ -0,0 +1,61 @@
+package Aula7;
+
+public class Carro {
+    private String cor;
+    private String marca;
+    private String modelo;
+    private int velocidade = 0;
+
+    public Carro(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String toString() {
+        return "Marca: " + marca + ", modelo: " + modelo + ", cor: " + cor;
+    }
+
+    public Carro(String modelo, String marca, String cor) {
+        this.modelo = modelo;
+        this.marca = marca;
+        this.cor = cor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        if (!modelo.isBlank()) {
+            this.modelo = modelo;
+        }
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public void buzinar() {
+        System.out.println("fom");
+    }
+
+    public int getVelocidade() {
+        return velocidade;
+    }
+
+    public void frear() {
+        System.out.println("rrrrrrr...");
+        this.velocidade = 0;
+    }
+}
---
+++ Aula7/CarroAcesso.java
@@ -0,0 +1,9 @@
+package Aula7;
+
+public class CarroAcesso {
+    public static void main(String[] args) {
+        var renegade = new Carro("Renegade", "Jeep", "Prata");
+        System.out.println(renegade.getMarca());
+
+    }
+}
