4a5bdcf309484e5fc15a248082479677dbbee327
Guilherme444 <guilherme.caetano1010@gmail.com>
2023-09-26 23:31:04 -0300

    Adicionando exercício de laços de repetição

--- ExerciciosLacosDeRepeticao/Exercicio5.java
+++ ExerciciosLacosDeRepeticao/Exercicio5.java
@@ -1,5 +1,31 @@
 package ExerciciosLacosDeRepeticao;
 
+import javax.swing.JOptionPane;
+
 public class Exercicio5 {
-    
+        public static void main(String[] args) {
+   
+        int n = Integer.parseInt(JOptionPane.showInputDialog("Digite o número de termos:"));
+
+   
+        double x = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor de x:"));
+
+        double resultado = calcularSerie(x, n);
+
+  
+        JOptionPane.showMessageDialog(null, "O resultado da série é: " + resultado);
+    }
+
+    public static double calcularSerie(double x, int n) {
+        double resultado = 0;
+        double sinal = 1;
+
+        for (int i = 1; i <= n; i++) {
+            double termo = ((Math.pow(x, 2 * i - 1)) / (i * (i - 1))) * sinal;
+            resultado += termo;
+            sinal = -sinal;
+        }
+
+        return resultado;
+    }
 }
