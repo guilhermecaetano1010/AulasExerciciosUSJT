50853ecfe4ade3738e899e82be3b13e91e0de2b7
Guilherme444 <guilherme.caetano1010@gmail.com>
2023-10-08 10:00:29 -0300

    Adicionando a aula 8

---
+++ Aula8/GerenciadorAluno.java
@@ -0,0 +1,71 @@
+package Aula8;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import java.awt.Font;
+
+public class GerenciadorAluno extends JFrame {
+
+    private Font font = new Font("Arial", Font.BOLD, 32);
+    JTextField login = new JTextField(30);
+    JPasswordField senha = new JPasswordField(30);
+
+    public static void main(String[] args) {
+        GerenciadorAluno ga = new GerenciadorAluno();
+        ga.desenhar();
+    }
+
+    public void desenhar() {
+        this.setSize(940, 240);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel painel = new JPanel();
+        this.add(painel);
+        painel.add(criarLabel("Login"));
+        login.setFont(font);
+        painel.add(login);
+        painel.add(criarLabel("Senha"));
+        painel.add(senha);
+        senha.setFont(font);
+        painel.add(criarBotaoCancelar());
+        JButton ok = new JButton("ok");
+        ok.addActionListener(acao -> {
+            if (login.getText().equals("root") &&
+                    senha.getText().equals("51")) {
+                CrudAluno ca = new CrudAluno();
+                ca.desenhar();
+                setVisible(false);
+            } else {
+                JOptionPane.showMessageDialog(this, "Login ou senha errado");
+            }
+
+        });
+        painel.add(ok);
+        ok.setFont(font);
+        this.setVisible(true);
+    }
+
+    JLabel criarLabel(String nome) {
+        JLabel loginLabel = new JLabel(nome);
+        loginLabel.setFont(font);
+        return loginLabel;
+    }
+
+    JButton criarBotaoCancelar() {
+        JButton cancelar = new JButton("cancelar");
+        cancelar.setFont(font);
+        cancelar.addActionListener(a -> {
+            login.setText("");
+            senha.setText("");
+        });
+        return cancelar;
+    }
+
+    public GerenciadorAluno() {
+        super("Login");
+    }
+}
