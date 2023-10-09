ded06bee63dc659b902c0fc6081ac6d182568c9c
Guilherme Caetano da Silva - 820140489 <820140489@ulife.com.br>
2023-10-09 20:06:03 -0300

    Criando regras dos métodos

--- Aula10/Personagem.java
+++ Aula10/Personagem.java
@@ -2,17 +2,29 @@ package Aula10;
 
 public class Personagem {
     String nome;
-    int energia;
-    int fome;
-    int sono;
+    int energia = 10;
+    int fome = 0;
+    int sono = 0;
 
 
     void cacar() {
-        System.out.println(nome + "caçando");
+        if(energia>=2){
+            System.out.println(nome + "caçando");
+            energia -= 2;
+        }
+        else{
+            System.out.println(nome + "sem energia para caçar");
+        }
+        fome = Math.min(fome + 1, 10);
+        sono = Math.min(sono + 1, 10);
     }
     
     void comer(){
-    System.out.println(fome + "comendo");
+    if (fome >= 1) {
+        System.out.println(fome + "comendo");
+        energia = Math.min(energia + 1, 10);
+        fome -= 1;
+    }
     }
 
     void dormir(){
