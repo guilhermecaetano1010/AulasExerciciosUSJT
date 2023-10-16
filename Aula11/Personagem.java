b62a6f6d53c21773dc3bdd4ae72de00aba95fa56
guilhermecaetano1010 <guilherme.caetano1010@gmail.com>
2023-10-16 19:39:53 -0300

    Adicionando as classes da aula 11

---
+++ Aula11/Personagem.java
@@ -0,0 +1,80 @@
+//void seria como um amigo, que não devolve nada. Não possui um retorno (return).
+
+public class Personagem {
+
+private String nome;
+private int energia = 10;
+private int fome = 0;
+private int sono = 0;
+
+
+public Personagem(String nome, int energia, int fome, int sono) {
+    this(energia, fome, sono);
+    this.nome = nome;
+}
+public Personagem( int energia, int fome, int sono) {
+   
+    if (energia >= 0 && energia <= 10){
+        this.energia = energia;
+    }
+    if (fome >= 0 && fome <= 10){
+        this.fome = fome;
+    }
+    if (sono >= 0 && sono <= 10){
+        this.sono = sono;
+    }
+}
+
+
+ void cacar(){
+    if (energia >= 2){
+    System.out.println(nome +" caçando");
+    energia -= 2;
+    } else {
+        System.out.println(nome + " sem energia para caçar");
+    }
+    fome = Math.min(fome + 1,10);
+    sono = Math.min(sono + 1,10);
+    estado();
+   
+}
+
+
+   
+void comer(){
+    if (fome >=1){
+    System.out.println(nome +" comendo");
+    energia = Math.min(energia + 1,10);
+    fome -= 1;
+    }
+    else{
+System.out.println(nome + " sem fome.");
+    }
+    estado();
+}
+
+
+
+
+void dormir(){
+    if (sono >=1){
+    System.out.println(nome +" dormindo");
+    sono -= 1;
+    energia = energia + 1 <= 10 ? energia + 1:10;
+    }
+else {
+    System.out.println(nome + " sem sono");
+    estado();
+    }
+    }
+    void estado(){
+        System.out.println(nome +" energia: " + energia +" fome: "+ fome +" sono: "+ sono + '\n');
+    }
+}
+
+
+
+    
+
+
+
